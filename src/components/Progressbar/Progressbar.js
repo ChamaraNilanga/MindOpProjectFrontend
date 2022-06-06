8fe738676b7ce6c85ff4e2a25dd490676629def8
ChamaraNilanga <chamaranilanga1999@gmail.com>
2022-06-06 16:56:22 +0530

    progress bar created

--- src/components/Progressbar/Progressbar.js
+++ src/components/Progressbar/Progressbar.js
@@ -1,15 +1,38 @@
-import React from "react";
-import propTypes from "prop-types";
-import "./Progressbar.css";
 
-function Progressbar({value}){
-    //need to pass the value from database progress precentge
-    return(
-        <progress className="progress-bar" value={value} max={100}></progress>
-    )
-}
-Progressbar.propTypes={
-    value : propTypes.number.isRequired,
+import React from 'react'
+
+const Progressbar = ({bgcolor,progress}) => {
+	
+	const Parentdiv = {
+		height: '4px',
+		width: '150px',
+		backgroundColor: '#b8b5b2',
+		borderRadius: 40,
+		margin: 10
+	}
+	
+	const Childdiv = {
+		height: '105%',
+		width: `${progress}%`,
+		backgroundColor: '#f5b042',
+	    borderRadius:40,
+		textAlign: 'right'
+	}
+	
+	const progresstext = {
+		padding: 1,
+		color: 'black',
+		fontsize:5
+	}
+		
+	return (
+	<div style={Parentdiv}>
+	<div style={Childdiv}>
+		{/* <span style={progresstext}>{`${progress}%`}</span> */}
+	</div>
+	</div>
+	)
 }
 
 export default Progressbar;
+
