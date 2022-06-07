18e0a8d8aae5d768feca58f36e4cd54838d41e2f
Rashmika Jayathilaka <63368361+Rashmika2020@users.noreply.github.com>
2022-06-08 00:07:50 +0530

    Student-side Chat

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Navigationbar from "./components/Navbar/Navbar"; 
+import ChatStudent from './components/ChatStudent/chat';
+
 
 function App() {
   return (
     <div className="App">
      <Router>
      <Navigationbar/>
+       <Routes>
+     {/* <Route path="/" element={<Navigationbar/>}/>    */}
+     <Route path="/student" element={<ChatStudent/>}/>
+   
+     </Routes>
      </Router>
     </div>
   );
