46fb54d58689789751ba765f748a63a7a9f0e8d5
bimsara <bmanjitha071@gmail.com>
2022-06-07 16:48:28 +0530

    created signin page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React ,{useState}from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import './App.css';
-import Navigationbar from "./components/Navbar/Navbar"; 
+import Navigationbar from "./components/Navbar/Navbar";
+
+import ForgetPasswordPage from './pages/forgetpassword.js'
+import HomePage from './pages/homepage.js'
+import SignInPage from './pages/SignInPage';
+
+
 
 function App() {
+  const [loggedIn,setLoggedIn]=useState(false)
   return (
     <div className="App">
      <Router>
-     <Navigationbar/>
+       {
+         loggedIn?(<>
+         <Navigationbar/>
+                  <Routes>
+                    <Route path="/home" element={< HomePage />} />
+                  </Routes>
+                 
+         </>):(<>
+          <Routes>
+          <Route path="/" element={ <SignInPage/>} />
+          <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+
+          </Routes>
+         
+         </>)
+
+       }
      </Router>
     </div>
   );
