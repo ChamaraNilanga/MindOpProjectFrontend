47141ba7553393bda2a902bbdb0d3a13df8d0b0c
bimsara <bmanjitha071@gmail.com>
2022-06-09 10:34:09 +0530

    created assignment and add some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AssignmentForm from './components/Navbar/AssignmentForm';
+import CreateAssignment from './pages/Assignment/CreateAssignment';
+import AssignmentDetails from './pages/Assignment/AssignmentDetails';
 
 
 ReactDOM.render(
