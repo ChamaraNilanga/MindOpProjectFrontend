3c190b3adc6852223ff101747d4b92813260bb77
ChamaraNilanga <chamaranilanga1999@gmail.com>
2022-06-12 13:27:47 +0530

    home page interface changed

---
+++ src/components/SubjectCardGrid/Subjectcardgrid.js
@@ -0,0 +1,39 @@
+import React , {useEffect,useState} from "react";
+import { Card } from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import axios from "axios";
+import Progressbar from "../Progressbar/Progressbar";
+import "./Subjectcardgrid.css";
+
+function Subjectcard ({courses , role , text}){
+     return(
+        <div className="overflow-auto">
+        <div className="subject-card-grid">
+            {courses.map(course=>{
+                
+                return(
+                    
+                    <Card style={{ width: '12rem' , height:'12rem'}} >
+                    <Card.Body>
+                    <Card.Title key={course.modid}>{course.modname}</Card.Title>
+                    <Card.Text className="text" key={course.modid}>
+                    {course.descrip}
+                    </Card.Text>
+                    
+                    </Card.Body>
+                    <Card.Footer>
+                        {role==='s' ? <Progressbar progress={course.progress}/> : role==='c' ? <></>:<Button className="btn" variant="warning" key={course.modid}>{text}</Button>}
+                        {/* <Button className="btn" variant="warning" key={course.modid}>Enroll</Button> */}
+                    </Card.Footer>
+                    </Card>
+                    
+                )
+            })}
+        </div>
+        </div>
+       
+        
+    )
+}
+
+export default Subjectcard;
