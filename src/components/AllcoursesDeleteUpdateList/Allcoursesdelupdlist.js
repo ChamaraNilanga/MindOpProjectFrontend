2ec59e95fcec4dd00a5a10eb266fe15c14825efe
ChamaraNilanga <chamaranilanga1999@gmail.com>
2022-06-15 22:23:28 +0530

    update delete course list

---
+++ src/components/AllcoursesDeleteUpdateList/Allcoursesdelupdlist.js
@@ -0,0 +1,46 @@
+import React , {useEffect , useState} from "react";
+import axios from "axios";
+import "./Allcoursesdelupdlist.css";
+
+function AllcoursesDelUpdlist (){
+    const [courses,setCourses] = useState([]);
+    
+    useEffect(()=>{
+        function getAllCourses(){
+            axios.get(`http://localhost:8070/coursedetails`)
+            .then((res)=>{
+                console.log(res);
+            setCourses(res.data);
+            }).catch((err)=>{
+                console.log(err);
+            })
+        }
+   
+    getAllCourses();
+},[])
+    return(
+        <div className="allcourselist">
+            <h3>All Courses In Learny</h3>
+            <div className="table table-striped table-light">
+            
+            {courses.map(course=>{
+                
+                return(
+                    <tbody>
+                        <tr key={course.modid}>
+                            <td class="w-25">{course.modname}</td>
+                            <td class="w-25">{course.modcode}</td>
+                            <td class="w-100">{course.descrip}</td>
+                            <td class="w-10"><button className="btn btn-warning">Update</button></td>
+                            <td class="w-15"><button className="btn btn-danger">Delete</button></td>
+                        </tr>
+                    </tbody>
+                    
+                )
+            })}
+        </div>
+        </div>
+    )
+}
+
+export default AllcoursesDelUpdlist;
