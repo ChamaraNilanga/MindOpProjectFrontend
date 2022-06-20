9af6ad0ac00d6bc68920b8d3b4aee962253af8a0
harsha sampath <86849661+Harshasampath@users.noreply.github.com>
2022-06-20 20:24:23 +0530

    content management & file management

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Navigationbar from "./components/Navbar/Navbar";
 import Allcourses from './components/Allcourses/Allcourses';
 import Homestudent from './pages/HomeStudent/HomeStudent';
 import Addcourseform from './pages/Addcourse/Addcourse';
-
+import CourseContent from './pages/CourseContent/CourseContent';
+import TopicBox from './components/TopicBox/TopicBox';
+import ViewVideos from './pages/ViewVideos/ViewVideos';
+import AddActivtyResoureses from './pages/AddActivity&Resoureses/AddActivtyResoureses';
 
 function App() {
   const role = 's';
@@ -17,6 +20,9 @@ function App() {
         <Routes>
           <Route path='/home' element={<Homestudent role={role}/>}/>
           <Route path='/course/addcourse' element={<Addcourseform/>}/>
+          <Route path='/coursecontent'element={<CourseContent/>}/>
+          <Route path='/viewvideos'element={<ViewVideos/>}/>
+          <Route path='/addactivityresourses'element={<AddActivtyResoureses/>}/>
         </Routes>
      </Router> 
     
---
+++ src/pages/ViewVideos/ViewVideos.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+import SubjectBar from '../../components/SubjectBar/SubjectBar'
+import VideoPlayer from '../../components/VideoPlayer/VideoPlayer'
+
+export default function ViewVideos() {
+  return (
+    <div>
+      <div>
+        <Navbar></Navbar>
+      </div>
+      <div>
+        <SubjectBar/>
+      </div>
+      <div className='Video'>
+        <VideoPlayer></VideoPlayer>
+      </div>
+    </div>
+  )
+}
