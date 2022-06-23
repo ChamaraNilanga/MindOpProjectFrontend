355557c673622bac8e14a855b1c91f5c9ab8dcc2
ChamaraNilanga <chamaranilanga1999@gmail.com>
2022-06-24 04:05:33 +0530

    Add working branch

--- src/components/ForumQuestionList/Forumquestionlist.js
+++ src/components/ForumQuestionList/Forumquestionlist.js
@@ -55,18 +55,21 @@ function Forumquestionlist({questions,name,mylist,user}){
             <h3>{name}</h3>
             <div className="dashboardadmin">
                 <ul>
-                {currentItems.map(question=>{
-                    return(
-                        <div>
-                             
-                            <li><Link to="/forum/single" state={{fid : question.fquestionid,name : question.name_, keyimage:question.image}}><p key={question.fquestionid} style={{color:'black',fontSize:'15px'}}>{question.name_}</p></Link></li>
-                            <div className="deletereplybox"><Link to="/forum/reply" state={{ fid : question.fquestionid, name : question.name_,keyimage:question.image}}><text className="textbold">Reply</text></Link>
-                            {mylist==true ? <i class="fa-solid fa-trash-can" style={{color:'red', marginLeft:'85%'}} onClick={()=>{deleteQuestion(question.fquestionid)}}></i> : <></>}
+                {currentItems!=null ? (
+                    currentItems.map(question=>{
+                        return(
+                            <div>
+                                 
+                                <li><Link to="/forum/single" state={{fid : question.fquestionid,name : question.name_, keyimage:question.image}}><p key={question.fquestionid} style={{color:'black',fontSize:'15px'}}>{question.name_}</p></Link></li>
+                                <div className="deletereplybox"><Link to="/forum/reply" state={{ fid : question.fquestionid, name : question.name_,keyimage:question.image}}><text className="textbold">Reply</text></Link>
+                                {mylist==true ? <i class="fa-solid fa-trash-can" style={{color:'red', marginLeft:'85%'}} onClick={()=>{deleteQuestion(question.fquestionid)}}></i> : <></>}
+                                </div>
+                                <hr/>
                             </div>
-                            <hr/>
-                        </div>
-                    )
-                })}
+                        )
+                    })
+                ):(<></>)}
+                
                 </ul>
                 <ReactPaginate
                     breakLabel="..."
